f2cd0c69fde1939b11ca63019263dd8a8524928d
Beka Westberg <bekawestberg@gmail.com>
2020-06-13 14:59:45 -0700

    Finalize code gen

--- appinventor/blocklyeditor/src/generators/yail/helpers.js
+++ appinventor/blocklyeditor/src/generators/yail/helpers.js
@@ -3,11 +3,11 @@
 // Released under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
-Blockly.Yail['helpers_dropdown'] = function(block) {
-  //var optionList = block.workspace.getComponentDatabase()
-      //.getOptionList(block.key_);
-  //var code = optionList.className + ":" +
-      //block.getFieldValue('OPTION');
-  var code = "com.google.appinventor.components.runtime.Component:DIRECTION";
+Blockly.Yail['helpers_dropdown'] = function() {
+  var optionList = this.workspace.getComponentDatabase()
+      .getOptionList(this.key_);
+  // See https://www.gnu.org/software/kawa/Enumerations.html
+  var code = optionList.className + ":" +
+      this.getFieldValue('OPTION');
   return [code, Blockly.Yail.ORDER_ATOMIC]
 }
